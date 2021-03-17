048cd860ce4fa5a0e63bfd6dd9324afe6239799f
Steven K <steven@pointcircle.com>
2021-03-17 13:20:20 -0700

    Updates for release script

--- bin/install_stack_builder.sh
+++ bin/install_stack_builder.sh
@@ -80,16 +80,19 @@ if ! [ -x "$(command -v python${PYTHON_BASE_VERSION})" ]; then
     cd /usr/src || echo "Unable to cd to /usr/src"
     sudo wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
     sudo tar xzf Python-${PYTHON_VERSION}.tgz
-    cd /usr/src/Python-${PYTHON_VERSION}  || echo "Unable to cd to /usr/src/Python-${PYTHON_VERSION}"; exit 255
+    cd /usr/src/Python-${PYTHON_VERSION}  || echo "Unable to cd to /usr/src/Python-${PYTHON_VERSION}"
     OPTS="--enable-optimizations"
     if [ ${ID} == 'centos' ] || [ ${ID} == 'amzn' ] || [ ${ID} == 'rhel' ]; then
         if [ ${VERSION_ID} == "6" ] || [ ${VERSION_ID} == "7" ]; then
             OPTS=""
         fi
     fi
-    sudo ./configure ${OPTS} || echo "Unable to configure ${OPTS}"; exit 255
-    sudo make < /dev/null  || echo "Unable to make"; exit 255
-    sudo make install < /dev/null  || echo "Unable to make install"; exit 255
+    echo "Running ./configure ${OPTS}"
+    sudo ./configure ${OPTS} || echo "Unable to configure ${OPTS}"
+    echo "Running make"
+    sudo make < /dev/null  || echo "Unable to make"
+    echo "Running make install"
+    sudo make install < /dev/null  || echo "Unable to make install"
     cd /usr/src || echo "Unable to cd to /usr/src"
     sudo rm -rf /usr/src/Python-${PYTHON_VERSION}
     sudo rm -f /usr/src/Python-${PYTHON_VERSION}.tgz
@@ -120,15 +123,15 @@ sudo -HE /usr/bin/pip${PYTHON_BASE_VERSION} install -q --upgrade uwsgi || echo "
 sudo -HE /usr/bin/pip${PYTHON_BASE_VERSION} install -q --upgrade dbus-python || echo "Unable to upgrade dbus-python"
 
 # Ensure we are good with github
-if ! [ $(id -u) = 0 ]; then
+if ! [ "$(id -u)" = "0" ]; then
     sudo -HE ssh-keygen -F github.com > /dev/null 2>&1 || \
-      ssh-keyscan github.com 2> /dev/null | sudo tee -a /root/.ssh/known_hosts > /dev/null && \
-      sudo chown root:root /root/.ssh/known_hosts && \
-      sudo chmod 640 /root/.ssh/known_hosts
+    ssh-keyscan github.com 2> /dev/null | sudo tee -a /root/.ssh/known_hosts > /dev/null && \
+    sudo chown root:root /root/.ssh/known_hosts && \
+    sudo chmod 640 /root/.ssh/known_hosts
     sudo -HE pip3 uninstall -qq infrastructure -y
     sudo -HE pip3 install -qq --upgrade --no-cache-dir git+ssh://git@github.com/icmanage/peercache-infrastructure.git || (c=$?; echo "Issue updating infrastructure"; (exit $c))
 else
-    ssh-keygen -F github.com > /dev/null 2>&1 || ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
+    ssh-keygen -F github.com > /dev/null 2>&1 || ssh-keyscan github.comssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
     pip3 uninstall -qq infrastructure -y
     pip3 install -qq --upgrade --no-cache-dir git+ssh://git@github.com/icmanage/peercache-infrastructure.git || (c=$?; echo "Issue updating infrastructure"; (exit $c))
 fi
